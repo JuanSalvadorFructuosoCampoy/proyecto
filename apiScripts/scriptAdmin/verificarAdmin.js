b68b92f162eb96c7007dcfcc694c88ede445f09f
Juansa <juansacampoy.train@gmail.com>
2024-02-26 19:41:54 +0100

    Añadido el script verificarAdmin.js en la gestión de los usuarios para verificar administrador

---
+++ apiScripts/scriptAdmin/verificarAdmin.js
@@ -0,0 +1,3 @@
+if(sessionStorage.getItem("rol")!= "admin"){
+    window.location.replace("index.html");
+}
--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ fetch("api/empleados.php", {
 })
 .then(response => response.json())
 .then(data => {
+    sessionStorage.setItem("rol", data.empleados[0].rol);
     if(data.empleados[0].rol == "admin"){
         const enlaceGestEmpl = document.createElement("A");
         enlaceGestEmpl.setAttribute("href", "apiScripts/empleados.html");
