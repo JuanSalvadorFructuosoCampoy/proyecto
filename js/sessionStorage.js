07e2ce51f39259f604ef48fcff969f8c7a228f0d
Juansa <juansacampoy.train@gmail.com>
2024-02-26 20:10:52 +0100

    Rutas relativas arregladas con window.location.host

--- js/sessionStorage.js
+++ js/sessionStorage.js
@@ -1,5 +1,10 @@
 // Objetivo: verificar si el usuario está autenticado antes de acceder a la página
 
 if(sessionStorage.getItem('token') == null){
-    window.location.href = 'autenticacion/login.html';
+    // Guarda el protocolo de la URL actual (http o https)
+    var protocol = window.location.protocol;
+    // Guarda el host de la URL actual (dominio y puerto)
+    var host = window.location.host;
+    // Redirige a la página de inicio de sesión concatenando el protocolo, el host y la ruta de la página de inicio de sesión
+    window.location.href = protocol + "//" + host + "/autenticacion/login.html";
 }
