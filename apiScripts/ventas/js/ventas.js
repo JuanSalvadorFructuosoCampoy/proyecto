d8fc9e8eedaf9e7db14043722ef59475d4fa0019
Juansa <juansacampoy.train@gmail.com>
2024-02-29 13:56:55 +0100

    Planteamiento de nueva venta

---
+++ apiScripts/ventas/js/ventas.js
@@ -0,0 +1,11 @@
+const categorias = document.querySelector("aside")
+console.log(categorias)
+
+const botonVolver = document.createElement("button")
+botonVolver.textContent = "Volver al inicio"
+botonVolver.classList.add("btn", "btn-primary", "position-fixed", "bottom-0", "end-0", "m-3")
+botonVolver.setAttribute("id", "volver")
+document.body.appendChild(botonVolver)
+botonVolver.addEventListener("click", () => {
+    window.location.href = "../../../index.html"
+})
