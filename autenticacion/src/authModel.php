561d96448f0bed5ff8c4f2b9228fe5c38e895a0d
Juansa <juansacampoy.train@gmail.com>
2024-04-26 08:33:47 +0200

    Añadidas credenciales de acceso a IONOS en comentarios

--- autenticacion/src/authModel.php
+++ autenticacion/src/authModel.php
@@ -19,6 +19,17 @@ class AuthModel
 			echo 'Error de conexión a la base de datos';
 			exit;
 		}
+
+	// 	$host_name = 'db5015623169.hosting-data.io';
+	// 	$database = 'dbs12758460';
+	// 	$user_name = 'dbu5544140';
+	// 	$password = 'MelodyLua2423';
+	// 	$this->connection = new mysqli($host_name, $user_name, $password, $database);
+
+	// 	if ($this->connection->connect_errno) {
+	// 		echo 'Error de conexión a la base de datos';
+	// 		exit;
+	//    }
 	}
 
 	/**
