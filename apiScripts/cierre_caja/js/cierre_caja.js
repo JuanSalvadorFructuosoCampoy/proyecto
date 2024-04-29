247d2003c92aaa992a10c50f43b3ac0a12ad4218
Juansa <juansacampoy.train@gmail.com>
2024-04-29 15:53:04 +0200

    Cambiada la posición del botón Volver en cierre caja y empleados

--- apiScripts/cierre_caja/js/cierre_caja.js
+++ apiScripts/cierre_caja/js/cierre_caja.js
@@ -97,7 +97,7 @@ fechaInput.addEventListener("input", () => {
  */
 const botonVolver = document.createElement("button")
 botonVolver.textContent = "Volver al inicio"
-botonVolver.classList.add("btn", "btn-primary", "position-fixed", "bottom-0", "start-0", "m-3")
+botonVolver.classList.add("btn", "btn-primary", "position-fixed", "bottom-0", "start-50", "m-3","translate-middle")
 botonVolver.setAttribute("id", "volver")
 document.body.appendChild(botonVolver)
 botonVolver.addEventListener("click", () => {
