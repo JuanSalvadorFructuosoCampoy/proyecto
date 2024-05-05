331a8294c8749fc2896dc9bc311e70e0cd300625
Juan Salvador Fructuoso Campoy <125875505+JuanSalvadorFructuosoCampoy@users.noreply.github.com>
2024-05-05 16:21:25 +0200

    Actualizar authModel.php

--- autenticacion/src/authModel.php
+++ autenticacion/src/authModel.php
@@ -20,10 +20,10 @@ class AuthModel
 			exit;
 		}
 
-	// 	$host_name = 'db5015623169.hosting-data.io';
-	// 	$database = 'dbs12758460';
-	// 	$user_name = ;
-	// 	$password = ;
+	// 	$host_name = '';
+	// 	$database = '';
+	// 	$user_name = '';
+	// 	$password = '';
 	// 	$this->connection = new mysqli($host_name, $user_name, $password, $database);
 
 	// 	if ($this->connection->connect_errno) {
